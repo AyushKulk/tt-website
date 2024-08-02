d000c58499617c1b5d8302e6bc74c4333173fb50
zohar le <zoharle@amazon.com>
2024-08-01 19:33:07 -0700

    Add page routers

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+        <h1>About</h1>
+        <p>Coming soon.</p>
+    </div>
+    )
+}
+
+export default About;
---
+++ src/pages/Brothers.jsx
@@ -0,0 +1,9 @@
+const Brothers = () => {
+    return (
+        <div>
+            <h1>Brothers Page</h1>
+        </div>
+    )
+};
+
+export default Brothers;
