b40127e9abcfa34773c562f941bad7759ebc9e09
LamPham123 <LamPham705@gmail.com>
2024-08-22 17:29:08 -0700

    small change test 2

--- src/pages/Recruitment.jsx
+++ src/pages/Recruitment.jsx
@@ -9,7 +9,7 @@ const Recruitment = () => {
                 <h1 className='header-style' style={{ fontSize: '40px' }}>What is Rush?</h1>
                 <p className='text-style'>Recruitment happens twice a year at the beginning of every 
                     Autumn and Spring quarter. Rush is a week long process where 4 events are held so 
-                    that the current members of Theta Tau can meet and get to know potential members!!!
+                    that the current members of Theta Tau can meet and get to know potential members!
                 </p>
 
             </div>
